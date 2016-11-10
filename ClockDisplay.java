e959a74bde8bec2da943b06a569d75afe01c07c1
jorgevff <jvalladaresfontano@gmail.com>
2016-11-10 12:11:17 +0100

    se crean las variables, objetos y se fija la hora

---
+++ ClockDisplay.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class ClockDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClockDisplay
+{
+    // declarar las variables de la clase NumberDisplay
+    private NumberDisplay hora;
+    
+    private NumberDisplay minuto;
+
+    /**
+     * se crean los objetos de la clase NumberDisplay 
+     */
+    public ClockDisplay()
+    {
+        // initialise instance variables
+        minuto = new NumberDisplay(60);
+        
+        hora = new NumberDisplay(24);
+       
+    }
+
+    /**
+     ahi que fijar la hora invocando el metodo getValue
+     */
+   public void fijarHora(int nuevaHora, int nuevoMinuto)
+    {
+        hora.setValue(nuevaHora);
+        minuto.setValue(nuevoMinuto);
+        
+    }
+}
